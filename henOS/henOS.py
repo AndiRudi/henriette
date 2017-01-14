7a61467a983180be991ac2d9c6abcf3cbe86247a
AndiRudi <Andreas@Rudischhauser.de>
2017-01-14 19:04:25 +0100

    x

---
+++ henOS/henOS.py
@@ -0,0 +1,28 @@
+import SimpleHTTPServer
+import SocketServer
+import lcddriver
+from datetime import datetime
+from time import *
+
+
+
+PORT = 8000
+
+# Main Application
+
+lcd = lcddriver.lcd()
+lcd.lcd_clear()
+lcd.lcd_display_string("henOS starting up", 1)
+lcd.lcd_display_string(datetime.now(), 2)
+
+
+# Use netifaces to get the IP
+
+
+
+
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+print "serving at port", PORT
+httpd.serve_forever()
