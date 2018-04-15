97983421d773e7a7b540c4bd19a99717ccd73110
AndiRudi <Andreas@Rudischhauser.de>
2018-04-15 18:21:11 +0200

    Added test core app

---
+++ henOS2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Unosquare.RaspberryIO;
+
+namespace henOS2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PI Hello World!");
+
+            Console.WriteLine(Pi.Info.ToString());
+
+        }
+    }
+}
