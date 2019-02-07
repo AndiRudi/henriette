211cc2f7efd604daecaab8d12a710910fb3ba692
AndiRudi <Andreas@Rudischhauser.de>
2019-02-07 23:37:09 +0100

    Added React2

--- henOS/henOS.py
+++ henOS/henOS.py
@@ -1,19 +1,22 @@
-import lcddriver
+#import lcddriver
 import netifaces
 import json
 from collections import deque
 from datetime import datetime
 from time import *
-from flask import Flask, url_for, jsonify
+from flask import Flask, url_for, jsonify, render_template, request 
 
-app = Flask(__name__)
+app = Flask(__name__, 
+            static_url_path='', 
+            static_folder='www/static',
+            template_folder='www/templates')
 
 thrust = 0
 rudder = 0
 
 @app.route('/')
 def api_root():
-    return 'Welcome to henOS'
+    return render_template("index.html", title = 'Henriette')
 
 @app.route('/overview')
 def api_overview():
@@ -26,7 +29,16 @@ def api_overview():
 
 @app.route('/thrust', methods=['POST'])
 def api_thrust():
-    return 'Engaged'
+
+    thrust = request.json['amount']
+    writeDebug("Thrust: " + str(thrust))
+
+    data = {
+        'thrust' : thrust,
+        'rudder' : rudder
+    }
+
+    return jsonify(data)
 
 @app.route('/rudder', methods=['POST'])
 def api_rudder():
@@ -45,26 +57,34 @@ def api_pump():
     return 'Navigating to'
 
 def writeDebug(text):
-    global lcdView
-    lcdView.popleft()
-    lcdView.append(text)
-    lcd.lcd_display_string(lcdView[0], 1)
-    lcd.lcd_display_string(lcdView[1], 2)
-    lcd.lcd_display_string(lcdView[2], 3)
-    lcd.lcd_display_string(lcdView[3], 4)
+    try:
+        global lcdView
+        lcdView.popleft()
+        lcdView.append(text)
+        lcd.lcd_display_string(lcdView[0], 1)
+        lcd.lcd_display_string(lcdView[1], 2)
+        lcd.lcd_display_string(lcdView[2], 3)
+        lcd.lcd_display_string(lcdView[3], 4)
+    except:
+        print(text)
 
 
 # Main Application
-lcdView = deque(['', '', '', ''])
-lcd = lcddriver.lcd()
-lcd.lcd_clear()
-writeDebug("henOS starting up")
-writeDebug(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+try:
+    lcdView = deque(['', '', '', ''])
+    lcd = lcddriver.lcd()
+    lcd.lcd_clear()
+    writeDebug("henOS starting up")
+    writeDebug(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+except:
+    print("Could not access display")
 
 # Use netifaces to get the IP
-addrs = netifaces.ifaddresses('wlan0')
-writeDebug("wlan0:" + str(addrs[netifaces.AF_INET][0]["addr"]))
-
+try:
+    addrs = netifaces.ifaddresses('wlan0')
+    writeDebug("wlan0:" + str(addrs[netifaces.AF_INET][0]["addr"]))
+except:
+    print("Could not access wlan interface")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int("80"), debug=True)
---
+++ henOS/www/static/js/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const e = React.createElement;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  thrust(amount) {
+    fetch('/thrust', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        amount: amount
+      })
+    });
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.thrust(10) },
+      'THRUST'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#app');
+ReactDOM.render(e(App), domContainer);
